63d3845abfac92a3c955c82120f75565f0f645fa
joaolotjr <joaolotjr@gmail.com>
2024-04-11 18:22:48 -0300

    add BD

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
-interface Pessoa {
-    name:string
-    age: number
-}
+import fastify from "fastify";
 
-console.log('hello World 3  ')
+const app = fastify()
+
+
+
+app.get('/', () =>{
+    return 'Hello NLW UNITE'
+
+})
+app.get('/teste', () =>{
+    return 'Hello TESTE'
+
+})
+
+app.listen({port:3333}).then(() =>{
+    console.log('HTTP server running!')
+
+})
