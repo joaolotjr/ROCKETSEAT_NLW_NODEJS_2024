5bc960f1a125c28f432286aab2686f1279b2c3a8
joaolotjr <joaolotjr@gmail.com>
2024-04-23 19:03:32 -0300

    change layout final GET

--- src/routes/get-event.ts
+++ src/routes/get-event.ts
@@ -11,12 +11,37 @@ export async function getEvent (app: FastifyInstance) {
                     params: z.object({
                         eventId: z.string(). uuid(),
                     }),
-                    response: {},
+                    response: {
+                        200: {
+                            event: z.object({
+                                id: z.string().uuid(),
+                                title: z.string(),
+                                slug: z.string(),
+                                details: z.string().nullable(),
+                                maximumAttendees: z.number().int().nullable(),
+                                attendeesAmount: z.number().int(),
+
+                            })
+                        }
+                    },
                 }
             }, async (request, reply) => {
                 const { eventId } = request.params
 
                 const event = await prisma.event.findUnique({
+                    select: {
+                        id: true,
+                        title: true,
+                         slug: true,
+                          details: true,
+                          maximumAttendees: true,
+                          _count: {
+                            select: {
+                                attendees: true,
+                            }
+                          }
+
+                    },
                     where: {
                         id:eventId,
                     }
@@ -24,7 +49,14 @@ export async function getEvent (app: FastifyInstance) {
                 if( event == null) {
                     throw new Error('Event not found')
                 }
-                return reply.send({ event })
+                return reply.send({ event: {
+                    id: event.id,
+                    title: event.title,
+                    slug: event.slug,
+                    details: event.details,
+                    maximumAttendees: event.maximumAttendees,
+                    attendeesAmount: event._count.attendees
+                } })
 
             }
 
