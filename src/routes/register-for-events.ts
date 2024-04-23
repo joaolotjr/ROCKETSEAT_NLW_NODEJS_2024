ac5441d02b8114e5575ab364f0ec5fa07dc2b08d
joaolotjr <joaolotjr@gmail.com>
2024-04-23 18:36:06 -0300

    error of limit of number of attendees per event

--- src/routes/register-for-events.ts
+++ src/routes/register-for-events.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
+import { promise, z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function registerForEvent(app: FastifyInstance) {
@@ -35,6 +35,25 @@ export async function registerForEvent(app: FastifyInstance) {
 
         if( attendeeFromEmail !== null ){
             throw new Error('This e-mail is already registered for this event.')
+        }
+
+        const [ event, amountOfAttendeesForEvent ] = await Promise.all([
+            prisma.event.findUnique({
+                where: {
+                    id: eventId,
+                }
+            }),
+
+            prisma.attendee.count({
+                where: {
+                    eventId,
+                }
+            })
+        ])        
+
+        if( event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees ){
+            throw new Error ('The maximum number of attendees for this event has been reached')
+
         }
 
         const attendee = await prisma.attendee.create({
