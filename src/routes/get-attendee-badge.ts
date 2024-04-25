c6789873e7b3901ed9aaef83eba04089431bde21
joaolotjr <joaolotjr@gmail.com>
2024-04-25 17:42:18 -0300

    add check-in for attendees

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -10,7 +10,20 @@ export async function getAttendeeBadge(app: FastifyInstance){
             params: z.object({
                 attendeeId: z.coerce.number().int(),
             }),
-            response: {},
+            response: {
+                200: z.object({
+                    badge: z.object({
+                        name: z.string(),
+                        email: z.string().email(),
+                        eventTitle: z.string(),
+                        checkInURL: z.string().url(),
+
+                    })
+                    
+
+                })
+
+            },
         }
     } , async ( request, reply ) => {
         const { attendeeId } = request.params
@@ -32,7 +45,24 @@ export async function getAttendeeBadge(app: FastifyInstance){
         if(attendee == null) {
             throw new Error('attendee not found.')
         }
-        return reply.send({ attendee })
+
+        
+
+        const baseURL = `${request.protocol}://${request.hostname}`
+
+        const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+
+
+
+        return reply.send({ 
+            badge: {
+                name:attendee.name,
+                email: attendee.email,
+                eventTitle: attendee.event.title,
+                checkInURL: checkInURL.toString(),
+            }
+         })
 
     })
     
