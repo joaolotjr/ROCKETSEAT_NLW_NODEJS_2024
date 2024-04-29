732c557077e213b03176243ce7aaf521128d1b7e
joaolotjr <joaolotjr@gmail.com>
2024-04-29 17:00:42 -0300

    add query and the event attendees

--- src/routes/get-event-attendees.ts
+++ src/routes/get-event-attendees.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import {  nullable, number, z } from "zod";
+import {  nullable, number, tuple, z } from "zod";
 import { prisma } from "../lib/prisma";
+import { create } from "domain";
+import { checkIn } from "./check-in";
 
 export async function getEventAttendees (app: FastifyInstance) {
     app
@@ -12,23 +14,71 @@ export async function getEventAttendees (app: FastifyInstance) {
                         eventId: z.string(). uuid(),
                     }),
                     querystring: z.object({
-                        pageIndex: z.string().nullable().default('0').transform(Number),
+                        query: z.string().nullish(),
+                        pageIndex: z.string().nullish().default('0').transform(Number),
                     }),
-                    response: {  },
+                    response: {
+                        200: z.object({
+                            attendees: z.array(
+                                z.object({
+                                    id: z.number(),
+                                    name: z.string(),
+                                    email: z.string(),
+                                    createdAt: z.date(),
+                                    checkedInAt: z.date().nullable()
+                                })
+                            )
+
+                        }) 
+                        
+                     },
                 }
             }, async (request, reply) => {
                 const { eventId } = request.params
-                const { pageIndex } = request.query
+                const { pageIndex, query } = request.query
 
                 const attendees = await prisma.attendee.findMany({
-                    where: {
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        createdAt: true,
+                        checkIn:{
+                            select: {
+                                createdAt: true
+                            }
+                        }
+                    },
+                    where: query ? {
                         eventId,
+                        name: {
+                            contains: query,
+                        }
+                    } : {
+                        eventId,                        
                     },
                     take: 10,
-                    skip: 
+                    skip: pageIndex * 10,
+                    orderBy: {
+                        createdAt: 'desc'
+                    }
                 })
 
-                return reply.send({ attendees })
+                return reply.send({
+                    attendees : attendees.map(attendee => {
+                        return {
+                            id: attendee.id,
+                            name: attendee.name,
+                            email: attendee.email,
+                            createdAt: attendee.createdAt,
+                            checkedInAt: attendee.checkIn?.createdAt ?? null
+
+
+                        }
+                        
+                    })
+
+                })
 
                 
 
